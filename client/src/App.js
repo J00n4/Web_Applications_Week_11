865e1d795e1c23103a2b6f17c291677910c78dc7
J00n4 <joona.ylijoki@gmail.com>
2021-11-30 13:02:26 +0200

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
 
+function onSubmit(event) {
+  event.preventDefault();
+  //const formData = new FormData(event.target);
+
+  const bookName = document.getElementById("name").value;
+  const bookAuthor = document.getElementById("author").value;
+  const bookPages = document.getElementById("pages").value;
+  fetch("/api/book", {
+    method: "POST",
+    body: {
+      name: bookName,
+      author: bookAuthor,
+      pages: bookPages
+    }
+    //body: formData,
+  })
+}
+
 function App() {
   return (
     <div className="App">
       <h1>books</h1>
+      <form id="submit_form" method="post">
+        <input type="string" id="name" />
+        <input type="string" id="author" />
+        <input type="number" id="pages" />
+        <input onClick={() => onSubmit()} type="submit" id="submit" />
+      </form>
     </div>
   );
 }
